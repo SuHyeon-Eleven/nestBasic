4ddbf7a26de28c5bc4c2c16b5c9156799a69a464
suhyeon <minsoohyeon1234@gmail.com>
2024-03-07 12:53:09 +0900

    [feat]: 환경변수 설정
    - configModuel 을 사용한 환경변수 관리
    - 개발 환경에 따른 환경변수 분리 위해 각각 파일 분리함
    - appModule 에 임포트

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import config from './configs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // env 파일 path
+      envFilePath: `${process.cwd()}/envs/${process.env.NODE_ENV}.env`,
+      // 커스텀 파일 ex).ts .yaml등
+      load: [config],
+      cache: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // NestFactory 호출 전에는 ConfigModuel 활성화 되지 않음.
+  const configService = app.get(ConfigService);
   await app.listen(3000);
+
+  console.log(`This server is running on ${configService.get('SERVICE_URL')}`);
 }
 bootstrap();
